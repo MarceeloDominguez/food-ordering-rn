c092510aa2348abb8bb4fcd12598bc3aa1322098
marcelo dominguez <marceelodominguez@gmail.com>
2024-08-14 12:39:01 -0300

    update order

--- app/(admin)/orders/[id].tsx
+++ app/(admin)/orders/[id].tsx
@@ -13,13 +13,14 @@ import OrderListItem from "@/components/OrderListItem";
 import OrderItemListItem from "@/components/OrderItemListItem";
 import { Colors } from "@/constants/Colors";
 import { OrderStatusList } from "@/types";
-import { useOrderDetails } from "@/api/orders";
+import { useOrderDetails, useUpdateOrder } from "@/api/orders";
 
 export default function OrderDetailsScreen() {
   const { id: idString } = useLocalSearchParams();
   const id = parseFloat(typeof idString === "string" ? idString : idString[0]);
 
   const { data: order, isLoading, error } = useOrderDetails(id);
+  const { mutate: updateOrder } = useUpdateOrder();
 
   //const order = orders.find((o) => o.id.toString() === id);
 
@@ -30,6 +31,10 @@ export default function OrderDetailsScreen() {
   if (error || !order) {
     return <Text>Faild to fetch</Text>;
   }
+
+  const updateStatus = (status: string) => {
+    updateOrder({ id: id, updatedFields: { status } });
+  };
 
   return (
     <View style={styles.container}>
@@ -48,7 +53,7 @@ export default function OrderDetailsScreen() {
               {OrderStatusList.map((status) => (
                 <Pressable
                   key={status}
-                  onPress={() => console.warn("Update status")}
+                  onPress={() => updateStatus(status)}
                   style={{
                     borderColor: Colors.light.tint,
                     borderWidth: 1,
